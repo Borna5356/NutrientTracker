38147a0b1717dc6ec43fa3e1d9839b4d9ccf832f
Borna5356 <bornakevin1@gmail.com>
2024-08-08 23:15:37 -0400

    Added getting users and validating passwords

--- backend/database.py
+++ backend/database.py
@@ -1,5 +1,5 @@
 import psycopg2
-import bcrypt
+import hashlib
 conn = psycopg2.connect(host="localhost", dbname="NutrientTracker", user="User",
                         password="Tehran14!", port=5432)
 
@@ -11,26 +11,66 @@ def create_user_table():
     query = """
     CREATE TABLE IF NOT EXISTS users(
     username VARCHAR(255) PRIMARY KEY,
-    password VARCHAR(255),
+    password TEXT NOT NULL,
     age INT,
     sex CHAR,
     weight int
     )    
     """
     cursor.execute(query)
+    conn.commit()
 
 def create_user(username, password, age, sex, weight):
-    hashed_password = bcrypt.hashpw(password, bcrypt.gensalt())
+    """
+    Hashes the password entered and adds the new user to the database
+    """
+    hashing = hashlib.sha256()
+    encoded_string = bytes(password, encoding="utf-8")
+    hashing.update(encoded_string)
+    hashed_password = str(hashing.digest())
+    password_array = hashed_password.split('\\')
+    hashed_password = ""
+    for string in password_array:
+        hashed_password += string
     query = """
     INSERT INTO users (username, password, age, sex, weight) 
     VALUES (%s, %s, %s, %s, %s)
     """
     values = [username, hashed_password, age, sex, weight]
     cursor.execute(query, values)
+    conn.commit()
+    return True
+
+def get_user(username):
+    """
+    gets a user's information if the username exists
+    """
+    query = "SELECT * FROM users WHERE username=%s"
+    values = [username]
+    cursor.execute(query, values)
+    conn.commit()
+    return cursor.fetchone()
+
+def validate_user(user, password):
+    """
+    makes sure that the password entered is 
+    """
+    hashing = hashlib.sha256()
+    encoded_string = bytes(password, encoding="utf-8")
+    hashing.update(encoded_string)
+    hashed_password = str(hashing.digest())
+    password_array = hashed_password.split('\\')
+    hashed_password = ""
+    for string in password_array:
+        hashed_password += string
+    correct_password = user[1]
+    return hashed_password == correct_password
 
 create_user_table()
-create_user('test', 'test', 2, 'm', 23)
-conn.commit()
+#create_user("Boobby", "test", 32, 'f', 324)
+user = get_user("Boobby")
+print(validate_user(user, "test"))
+
 
 cursor.close()
 conn.close()
