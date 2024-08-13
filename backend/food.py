d88ad6bbc3b9774b26f309bf897ca051823b45bb
Borna5356 <bornakevin1@gmail.com>
2024-08-13 12:33:58 -0400

    Able to create a food table and create and add food to the table

--- backend/food.py
+++ backend/food.py
@@ -5,9 +5,9 @@ conn = psycopg2.connect(host="localhost", dbname="NutrientTracker", user="User",
 
 cursor = conn.cursor()
 
-def create_tracked_calories_table():
+def create_food_table():
     query = """
-    CREATE TABLE IF NOT EXISTS food (
+    CREATE TABLE IF NOT EXISTS foods (
     name TEXT PRIMARY KEY,
     total_calories INT,
     protein INT,
@@ -19,7 +19,17 @@ def create_tracked_calories_table():
     cursor.execute(query)
     conn.commit()
 
+def create_food(name, calories, protein, carbs, sugars, fats):
+    query = """
+    INSERT INTO foods (name, total_calories, protein, carbs, sugars, fats) VALUES
+    (%s, %s, %s, %s, %s, %s)
+    """
+    values = [name, calories, protein, carbs, sugars, fats]
+    cursor.execute(query, values)
+    conn.commit()
 
-create_tracked_calories_table()
+
+create_food_table()
+create_food("burger", 300, 23, 23, 34, 45)
 cursor.close()
 conn.close()
